c32930c44a68e37d27a5301f524bc659f5beab0c
FYuyaS <fysato@estudante.ufscar.br>
2026-04-24 18:59:28 -0300

    Começando

---
+++ exercicio1/force-app/main/default/lwc/editorConta/editorConta.js
@@ -0,0 +1,16 @@
+import { LightningElement, api } from 'lwc';
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+import NAME_FIELD from '@salesforce/schema/Account.Name';
+import PHONE_FIELD from '@salesforce/schema/Account.Phone';
+import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
+
+export default class EditorConta extends LightningElement {
+    @api recordId;
+
+    handleChange(event){
+        
+    }
+    
+
+
+}
